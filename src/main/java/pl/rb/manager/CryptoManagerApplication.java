0e9271de3ad2d041940bdf906922bbb1eefaf32c
rafalbanasiewicz <banasiewicz969696@gmail.com>
2022-03-11 14:45:15 +0100

    init

---
+++ src/main/java/pl/rb/manager/CryptoManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.rb.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rb/manager/CryptoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rb.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
