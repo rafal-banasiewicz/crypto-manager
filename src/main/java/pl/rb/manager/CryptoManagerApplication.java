f210c596826ddc82aba74bd16b10b6ec60f9b4e5
rafalbanasiewicz <banasiewicz969696@gmail.com>
2023-04-02 00:55:23 +0200

    remove unused comments

--- src/main/java/pl/rb/manager/CryptoManagerApplication.java
+++ src/main/java/pl/rb/manager/CryptoManagerApplication.java
@@ -1,7 +1,5 @@
 package pl.rb.manager;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,11 +10,6 @@ public class CryptoManagerApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(CryptoManagerApplication.class, args);
-//        OkHttpClient client = new OkHttpClient();
-//        String URL = "http://localhost:8082/getInstruments";
-//        Request request = new Request.Builder().url(URL).get().build();
-//        String response = client.newCall(request).execute().body().string();
-//        System.out.println(response);
     }
 
 }
